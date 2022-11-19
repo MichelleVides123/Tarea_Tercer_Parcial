443823312c14c4a8aeb562d98e568d5b61c4b004
MichelleVides123 <demaryoryorellana2112@gmail.com>
2022-11-18 19:17:45 -0600

    parte uno
    creando el proyecto y repositorio

---
+++ Aplicacion Web/Datos/Interfaces/ILogingRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelos;
+
+namespace Datos.Interfaces
+{
+    public interface ILogingRepositorio
+    {
+        Task<bool> ValidacionUsuario(Login login);
+    }
+}
